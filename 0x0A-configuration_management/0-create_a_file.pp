281018b111c50e885ea046c6094dec70a5237988
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-08-25 23:39:46 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,9 @@
 
 file { 'tmp/school':
   ensure => file,
+  path => 'tmp/school',
   mode => '0744',
   owner => 'www-data',
   group => 'www-data',
-  path => 'tmp/school',
   content => 'I love Puppet'
 }
